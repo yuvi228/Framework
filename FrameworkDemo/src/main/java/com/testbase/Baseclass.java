981c90cde7a91b924a3e04fc9879a2d9e65b0a3d
Swap Portal <swapportalsry@gmail.com>
2019-11-03 14:02:55 +0530

    Remove spacing

--- FrameworkDemo/src/main/java/com/testbase/Baseclass.java
+++ FrameworkDemo/src/main/java/com/testbase/Baseclass.java
@@ -9,9 +9,6 @@ public class Baseclass {
 
 	public static WebDriver driver; // To invoke Webdriver in Selenium
 	// public static IOSDriver<MobileElement> driver; To invoke iOS driver in Appium
-	
-
-
 
 	public void init() {
 		// TODO Auto-generated constructor stub
